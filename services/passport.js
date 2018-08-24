72bf15d773883dc981a5de0ef146ef992cc6187c
TheBestDon <donatas_bakanas@yahoo.com>
2018-08-24 21:01:00 +0300

    mongoDB connected

---
+++ services/passport.js
@@ -0,0 +1,21 @@
+const passport = require("passport");
+const mongoose = require("mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+
+const User = mongoose.model("User");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      new User({
+        googleId: profile.id
+      }).save();
+    }
+  )
+);
