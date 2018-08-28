2069459a07fbbfa695a8138fb06ef52284eed6f7
TheBestDon <donatas_bakanas@yahoo.com>
2018-08-28 13:29:59 +0300

    addind mailer

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 const keys = require("./config/keys");
 const bodyParser = require("body-parser");
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 mongooge.connect(
@@ -29,6 +30,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets like our main.js or main.css
