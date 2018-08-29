72b8571dd830d7ed354ef8eb0f9c78543d461e51
TheBestDon <donatas_bakanas@yahoo.com>
2018-08-29 18:33:30 +0300

    surveys added

---
+++ client/src/reducers/surveysReducer.js
@@ -0,0 +1,12 @@
+import { FETCH_SURVEYS } from "../actions/types";
+
+const initialState = [];
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_SURVEYS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
